31bcf1688b230ce4d40cc3fd5ca43e3b5cc357f3
Justin Brumley <justinbrumley012@gmail.com>
2018-03-13 18:32:28 -0500

    Added connection to the server for setup. Also added Megaman.

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"github.com/gorilla/websocket"
+	"net/http"
 	"os"
 )
 
@@ -19,4 +21,59 @@ func main() {
 	}
 
 	fmt.Printf("Connecting to server with local url %v and name %v\n", local, name)
+
+	dialer := &websocket.Dialer{
+		ReadBufferSize:  1024,
+		WriteBufferSize: 1024,
+	}
+
+	header := &http.Header{}
+	header.Add("Origin", "http://localhost")
+	conn, _, err := dialer.Dial("ws://localhost:8080/ws", *header)
+	if err != nil {
+		fmt.Printf("Error connecting to server: %v\n", err)
+		return
+	}
+
+	// Write the chosen name to the server to get back a url to use as webhook
+	err = conn.WriteMessage(websocket.TextMessage, []byte(name))
+	if err != nil {
+		fmt.Printf("Error sending message to server: %v\n", err)
+		return
+	}
+
+	// Receive full url back from server
+	_, message, err := conn.ReadMessage()
+	if err != nil {
+		fmt.Printf("Error reading message from server: %v\n", err)
+		return
+	}
+
+	fmt.Printf(`
+░░░░░░░░░░▄▄█▀▀▄░░░░
+░░░░░░░░▄█████▄▄█▄░░░░
+░░░░░▄▄▄▀██████▄▄██░░░░
+░░▄██░░█░█▀░░▄▄▀█░█░░░▄▄▄▄
+▄█████░░██░░░▀▀░▀░█▀▀██▀▀▀█▀▄
+█████░█░░▀█░▀▀▀▀▄▀░░░███████▀
+░▀▀█▄░██▄▄░▀▀▀▀█▀▀▀▀▀░▀▀▀▀
+░▄████████▀▀▀▄▀░░░░
+██████░▀▀█▄░░░█▄░░░░
+░▀▀▀▀█▄▄▀░██████▄░░░░
+░░░░░░░░░█████████░░░░
+	`)
+	fmt.Printf("\nAll traffic from the following url: \n\n\t%v\n\nwill be forwarded to your local url:\n\n\t%v\n\n", string(message), local)
+
+	// Now we wait
+	for {
+		messageType, message, err := conn.ReadMessage()
+		if err != nil {
+			fmt.Printf("Server error: %v\n", err)
+			break
+		}
+
+		if messageType == websocket.TextMessage {
+			fmt.Printf("Received message from server: \n%v\n", string(message))
+		}
+	}
 }
